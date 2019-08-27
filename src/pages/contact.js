3f467735423581893f66dfc103602e5f7658d558
Sherry Li <sherryhli@users.noreply.github.com>
2019-08-27 15:14:17 -0400

    Integrate Formspree for contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,9 +7,10 @@ const ContactPage = () => {
     return (
         <Layout>
             <SEO title="Contact" />
-            <form action="https://formspree.io/sherryhli@hotmail.com" method="POST" id="contact-form">
-                <input type="email" name="reply-to"></input>
-                <textarea form="contact-form" name="message"></textarea>
+            <p>To contact us, fill in the form below or email <a href="mailto:president@onqba.ca">president@onqba.ca</a> or <a href="secretary@onqba.ca">secretary@onqba.ca</a>. You can also message us on <a href="https://twitter.com/OntarioQuizbowl">Twitter</a> or <a href="https://www.facebook.com/ONQBA/">Facebook</a>.</p>
+            <form action="https://formspree.io/sherryhli@hotmail.com" method="POST" id="contact-form" className="contact-form">
+                <input type="email" name="reply-to" placeholder="Your email address"></input>
+                <textarea form="contact-form" name="message" rows="15" cols="50" placeholder="Your message"></textarea>
                 <input type="submit" name="submit"></input>
             </form>
         </Layout>
