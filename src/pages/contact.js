9ba448505b455c86c494d4339d9ec3a3553762d9
Sherry Li <sherryhli@users.noreply.github.com>
2019-08-27 20:59:14 -0400

    Decrease size of message box in contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const ContactPage = () => {
             <p>To contact us, fill in the form below or email <a href="mailto:president@onqba.ca">president@onqba.ca</a> or <a href="secretary@onqba.ca">secretary@onqba.ca</a>. You can also message us on <a href="https://twitter.com/OntarioQuizbowl">Twitter</a> or <a href="https://www.facebook.com/ONQBA/">Facebook</a>.</p>
             <form action="https://formspree.io/meghan.torchia@gmail.com" method="POST" id="contact-form" className="contact-form">
                 <input type="email" name="reply-to" placeholder="Your email address"></input>
-                <textarea form="contact-form" name="message" rows="15" cols="50" placeholder="Your message"></textarea>
+                <textarea form="contact-form" name="message" rows="12" cols="50" placeholder="Your message"></textarea>
                 <input type="submit"></input>
             </form>
         </Layout>
