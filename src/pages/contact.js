55ba2f0ca3df61678e7287960468e1f74311e6b7
Sherry Li <sherryhli@users.noreply.github.com>
2019-08-29 00:10:10 -0400

    Edit contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,10 +7,10 @@ const ContactPage = () => {
     return (
         <Layout>
             <SEO title="Contact" />
-            <p>To contact us, fill in the form below or email <a href="mailto:president@onqba.ca">president@onqba.ca</a> or <a href="secretary@onqba.ca">secretary@onqba.ca</a>. You can also message us on <a href="https://twitter.com/OntarioQuizbowl">Twitter</a> or <a href="https://www.facebook.com/ONQBA/">Facebook</a>.</p>
+            <p>To contact us, fill in the form below. You can also message us on <a href="https://twitter.com/OntarioQuizbowl">Twitter</a> or <a href="https://www.facebook.com/ONQBA/">Facebook</a>.</p>
             <form action="https://formspree.io/meghan.torchia@gmail.com" method="POST" id="contact-form" className="contact-form">
-                <input type="email" name="reply-to" placeholder="Your email address"></input>
-                <textarea form="contact-form" name="message" rows="12" cols="50" placeholder="Your message"></textarea>
+                <input type="email" name="reply-to" placeholder=" Your email address"></input>
+                <textarea form="contact-form" name="message" rows="12" cols="50" placeholder=" Your message"></textarea>
                 <input type="submit"></input>
             </form>
         </Layout>
